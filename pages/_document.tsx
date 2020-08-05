f32e1068e65c9dd54762b2fbcc0020cedf24a140
Cam Pedersen <cam@campedersen.com>
2020-08-05 17:13:07 -0400

    add typescript

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,7 +1,8 @@
-import Document from "next/document";
+import Document, { DocumentContext } from "next/document";
 import { ServerStyleSheet } from "styled-components";
+
 export default class MyDocument extends Document {
-  static async getInitialProps(ctx) {
+  static async getInitialProps(ctx: DocumentContext) {
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
     try {
@@ -14,12 +15,7 @@ export default class MyDocument extends Document {
 
       return {
         ...initialProps,
-        styles: (
-          <>
-            {initialProps.styles}
-            {sheet.getStyleElement()}
-          </>
-        ),
+        styles: [initialProps.styles, sheet.getStyleElement()],
       };
     } finally {
       sheet.seal();
